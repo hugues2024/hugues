73b4ab981dd774a6f2969af74d005b8a9ab1e6c4
Hugues <huguesmariehounkpatin1@gmail.com>
2026-02-10 00:27:18 +0100

    Test google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ReactGA from "react-ga4";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -6,6 +7,13 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Portfolio from './pages/portfolio/Portfolio';
 import Contact from './pages/contact/Contact';
+
+
+// Initialise Google Analytics avec ton ID de mesure
+ReactGA.initialize("G-XXXXXXXXXX"); // REMPLACE PAR TON ID
+
+// Envoie une notification de vue de page pour la page d'accueil
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 function App() {
   return (
