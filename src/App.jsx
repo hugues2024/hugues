f5b299c49f5e25e810daa83365bbf12b9577965e
Hugues <huguesmariehounkpatin1@gmail.com>
2026-02-10 00:33:13 +0100

    importation de reacr-ga4 dans le main et les autres configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import ReactGA from "react-ga4";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -8,12 +7,6 @@ import About from './pages/about/About';
 import Portfolio from './pages/portfolio/Portfolio';
 import Contact from './pages/contact/Contact';
 
-
-// Initialise Google Analytics avec ton ID de mesure
-ReactGA.initialize("G-XXXXXXXXXX"); // REMPLACE PAR TON ID
-
-// Envoie une notification de vue de page pour la page d'accueil
-ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 function App() {
   return (
